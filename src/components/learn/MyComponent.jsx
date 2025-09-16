f698a2bcc12913c1e7d43c6868933aa223314a6f
tranmanhthangg <thangtranhehe3456@gmail.com>
2025-09-16 22:49:26 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,20 @@
 import "./style.css"
 
 const MyComponent = () => {
+    // const hoidanit = "Eric";
+    // const hoidanit = 25;
+    // const hoidanit = true;
+    // const hoidanit = [1, 2, 3];
+
+    const hoidanit = {
+        name: "Eric",
+        age: 25
+    };
+
     return (
         <>
-            <div>eric & hoi dan it</div>
+            <div>{JSON.stringify(hoidanit)} & hoi dan it</div>
+            <div>{console.log("Eric")}</div>
             <div className="child" style={{ borderRadius: " 10px" }}>child</div>
         </>
     );
