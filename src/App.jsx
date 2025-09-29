3d13df9b2f129a1345be30070fa5a3b565ab2e42
tranmanhthangg <thangtranhehe3456@gmail.com>
2025-09-29 21:47:43 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { useState } from "react";
 
 const App = () => {
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React" },
-    { id: 2, name: "Watching Yotube" }
+    // { id: 1, name: "Learning React" },
+    // { id: 2, name: "Watching Yotube" }
   ]);
 
   const addNewTodo = (name) => {
@@ -27,10 +27,14 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData todoList={todoList} />
-      <div className="todo-image">
-        <img className="logo" src={reactLogo} />
-      </div>
+      {todoList.length > 0 ?
+        <TodoData todoList={todoList} />
+        :
+        <div className="todo-image">
+          <img className="logo" src={reactLogo} />
+        </div>
+      }
+
     </div>
   )
 }
