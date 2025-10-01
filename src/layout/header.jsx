b42abed5e91c4171dcef4067b4339d2bab60a9b3
tranmanhthangg <thangtranhehe3456@gmail.com>
2025-10-02 03:30:58 +0700

    video 49

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,11 +1,13 @@
 import './header.css';
 
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
         <ul>
-            <li><a className="active" href="#home">Home</a></li>
-            <li><a href="/users">User</a></li>
-            <li><a href="/products">Product</a></li>
+            <li><Link className="active" to="#home">Home</Link></li>
+            <li><Link to="/users">User</Link></li>
+            <li><Link to="/products">Product</Link></li>
         </ul>
     );
 }
