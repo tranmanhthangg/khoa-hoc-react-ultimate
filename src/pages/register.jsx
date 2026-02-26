2f0adc7e89f411ebe48febf0eae66c73b05ca860
tranmanhthangg <thangtranhehe3456@gmail.com>
2026-02-27 02:48:25 +0700

    video 91

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form, notification } from 'antd';
+import { Button, Input, Form, notification, Row, Col } from 'antd';
 import { registerUserAPI } from '../service/api.service';
 import { useNavigate } from 'react-router-dom';
 
@@ -31,39 +31,57 @@ const RegisterPage = () => {
             onFinish={onFinish}
         // onFinishFailed={onFinishFailed}
         >
-            <div style={{ margin: "50px" }}>
-                <Form.Item
-                    label="Full Name:"
-                    name="fullName"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Email:"
-                    name="email"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Password:"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item
-                    label="Phone Number:"
-                    name="phone"
-                    rules={[{ pattern: new RegExp(/\d+/g), message: 'Wrong format!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <div>
-                    <Button type='primary' onClick={() => form.submit()}>Register</Button>
-                </div>
-            </div>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Full Name:"
+                        name="fullName"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Email:"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Password:"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Phone Number:"
+                        name="phone"
+                        rules={[{ pattern: new RegExp(/\d+/g), message: 'Wrong format!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8} >
+                    <div>
+                        <Button type='primary' onClick={() => form.submit()}>Register</Button>
+                    </div>
+                </Col>
+            </Row>
         </Form>
     );
 }
